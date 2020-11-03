7ee1179495e8372c79af67bbd8e5db632e35761f
Simon Schork <simon.schork@gmail.com>
2020-11-03 10:25:52 +0100

    last commit

--- PHP_MySQL/initial/meldung.php
+++ PHP_MySQL/initial/meldung.php
@@ -9,20 +9,33 @@
   <h2>Von Aliens entführt – eine Entführung melden</h2>
 
 <?php
-  $wann_geschah_es = $_POST['wanngeschahes'];
+  $vorname = $_POST['vorname'];
+  $nachname = $_POST['nachname'];
+  $wie_viele = $_POST['wieviele'];
+  $was_sie_taten = $_POST['wassietaten'];
+  $weiteres = $_POST['weiteres'];
+  $wann_es_geschah = $_POST['wanngeschahes'];
   $wie_lange = $_POST['wielange'];
-  $alien_beschreibung = $_POST['beschreibung'];
+  $alien_beschreibung = $_POST['alienbeschreibung'];
   $fang_gesehen = $_POST['fanggesehen'];
   $email = $_POST['email'];
  
+  $db = mysqli_connect('localhost','root','','aliendatenbank') or die('Fehler beim Verbinden mit MYSQL-Server.');
 
+  $sql = "INSERT INTO alien_entfuehrung VALUES ('$vorname', '$nachname', '$wann_es_geschah', '$wie_lange', '$wie_viele', '$alien_beschreibung', '$was_sie_taten', '$fang_gesehen', '$weiteres', '$email')";
+  $ergebnis = mysqli_query($db, $sql) or die('Fehler bei Datenabfrage.');
+
+  mysqli_close($db);
   
   echo 'Vielen Dank für den Bericht.<br />'; 
-  echo 'Sie wurden ' . $wann_geschah_es;
+  echo 'Sie wurden ' . $wann_es_geschah;
   echo ' entführt und waren ' . $wie_lange . ' weg<br />';
+  echo 'Anzahl Aliens: '.$wie_viele.'<br />';
   echo 'Beschreiben Sie sie: ' . $alien_beschreibung . '<br />';
+  echo 'Was die Aliens machten: '.$was_sie_taten . '<br />';
   echo 'Was die Aliens machten: ' . $was_sie_taten . '<br />';
   echo 'War Fang da? ' . $fang_gesehen . '<br />';
+  echo 'Weitere Bemerkungen: ' . $weiteres . '<br />';
   echo 'Ihre E-Mail-Adresse: ' . $email;
 ?>
 
